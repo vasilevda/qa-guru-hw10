114b314719d1d8ecaec197d6ec5c9c1524e762bd
da.vasilyev <dmitry.vasilyew@gmail.com>
2022-02-01 20:01:20 +0300

    fixed DemoqaFormTest, added annotation owner

--- src/test/java/com/severstal/DemoqaFormTest.java
+++ src/test/java/com/severstal/DemoqaFormTest.java
@@ -2,12 +2,14 @@ package com.severstal;
 
 import com.github.javafaker.Faker;
 import com.severstal.pages.PracticeFormPage;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
+@Owner("da-vasilev@ya.ya")
 public class DemoqaFormTest extends TestBase{
     private static final Faker faker = new Faker();
     private static final TestData testData = new TestData();
